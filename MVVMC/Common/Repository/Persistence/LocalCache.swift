1244917e110cd6f9378dc88ee4a752b561127b9b
infinity0629 <1185561323@qq.com>
2023-10-28 00:18:21 +0800

    update

---
+++ MVVMC/Common/Repository/Persistence/LocalCache.swift
@@ -0,0 +1,18 @@
+//
+//  LocalCache.swift
+//  MVVMC
+//
+//  Created by sam on 2023/10/27.
+//
+
+import Foundation
+import Cache
+
+public final class LocalCache {
+    
+    public static let shared = LocalCache()
+    
+    private init() {
+
+    }
+}
---
+++ MVVMC/Common/Repository/Remote/NetworkClient.swift
@@ -0,0 +1,48 @@
+//
+//  NetworkClient.swift
+//  MVVMC
+//
+//  Created by sam on 2023/10/27.
+//
+
+import RxSwift
+import Moya
+import MoyaSugar
+import Moya_SwiftyJSONMapper
+
+public final class NetworkClient {
+    
+    public var timeoutInterval: TimeInterval = 30
+    
+    public var plugins: [PluginType] = [
+        NetworkLoggerPlugin(configuration: NetworkLoggerPlugin.Configuration(logOptions: .verbose))
+    ]
+    
+    public static let shared = NetworkClient()
+    
+    private init() {
+
+    }
+    
+    public func request<TargetType: SugarTargetType, ParseType: JSONEntity>(_ token: TargetType,
+                                                                            parseType: ParseType.Type,
+                                                                            callbackQueue: DispatchQueue? = nil) -> Single<ParseType>  {
+        makeProvider().rx
+            .request(token, callbackQueue: callbackQueue)
+            .map(to: parseType)
+    }
+    
+    public func request<TargetType: SugarTargetType, ParseType: JSONEntity>(_ token: TargetType,
+                                                                            parseType: [ParseType.Type],
+                                                                            callbackQueue: DispatchQueue? = nil) -> Single<[ParseType]>  {
+        makeProvider().rx
+            .request(token, callbackQueue: callbackQueue)
+            .map(to: parseType)
+    }
+    
+    private func makeProvider<TargetType: SugarTargetType>() -> MoyaSugarProvider<TargetType> {
+        let session = MoyaSugarProvider<TargetType>.defaultAlamofireSession()
+        session.sessionConfiguration.timeoutIntervalForRequest = timeoutInterval
+        return MoyaSugarProvider<TargetType>(session: session, plugins: plugins)
+    }
+}
