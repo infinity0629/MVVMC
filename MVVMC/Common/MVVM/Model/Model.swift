a9556f6023fed5c8220d3cff4593a0666529dd16
infinity0629 <1185561323@qq.com>
2023-10-27 17:34:59 +0800

    udpate

--- MVVMC/Common/MVVM/Model/Model.swift
+++ MVVMC/Common/MVVM/Model/Model.swift
@@ -6,31 +6,5 @@
 //
 
 import Foundation
-import RxSwift
-import Moya
-import MoyaSugar
-import Moya_SwiftyJSONMapper
 
 public protocol Model {}
-
-private let timeoutInterval: TimeInterval = 30
-
-private let plugins: [PluginType] = [
-    NetworkLoggerPlugin(configuration: NetworkLoggerPlugin.Configuration(logOptions: .verbose))
-]
-
-extension Model {
-    
-    func request<TargetType: SugarTargetType, ParseType: ALSwiftyJSONAble>(_ token: TargetType, 
-                                                                           parseType: ParseType.Type,
-                                                                           timeoutInterval: TimeInterval = timeoutInterval,
-                                                                           callbackQueue: DispatchQueue? = nil) -> Single<ParseType>  {
-        
-        let session = MoyaSugarProvider<TargetType>.defaultAlamofireSession()
-        session.sessionConfiguration.timeoutIntervalForRequest = timeoutInterval
-        let provider = MoyaSugarProvider<TargetType>(session: session, plugins: plugins)
-        return provider.rx
-            .request(token, callbackQueue: callbackQueue)
-            .map(to: parseType)
-    }
-}
