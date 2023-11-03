45214864396f4dee5f19497db571a4c27fb2bf90
infinity0629 <1185561323@qq.com>
2023-11-03 11:47:27 +0800

    update

--- MVVMC/Module/Product/Model/ProductEntity.swift
+++ MVVMC/Module/Product/Model/ProductEntity.swift
@@ -5,14 +5,6 @@
 //  Created by sam on 2023/10/29.
 //
 
-//import SwiftyJSON
-//
-//struct ProductEntity: INFJSONEntity {
-//    init?(_ json: JSON) {
-//        nil
-//    }
-//}
-
 struct ProductEntity {
 
 }
